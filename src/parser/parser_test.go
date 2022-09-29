4696f8b70d86337dcfde1634bfbcf88672e81a0e
Aaron Price <aaron.price@hurricanecommerce.com>
2022-09-29 15:43:20 +0100

    parser: simplify tests
    	modified:   src/parser/parser_test.go

--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -26,10 +26,8 @@ func TestParser(t *testing.T) {
 			}
 
 			p := New(lexer.New(input))
-			actual := p.ParseProgram()
 
-			assert.Nil(t, p.Errors())
-			assert.Equal(t, expect, actual)
+			doTest(t, p, expect)
 		})
 
 		t.Run("let y = 10;", func(t *testing.T) {
@@ -46,10 +44,8 @@ func TestParser(t *testing.T) {
 			}
 
 			p := New(lexer.New(input))
-			actual := p.ParseProgram()
 
-			assert.Nil(t, p.Errors())
-			assert.Equal(t, expect, actual)
+			doTest(t, p, expect)
 		})
 
 		t.Run("let foobar = 838383;", func(t *testing.T) {
@@ -66,33 +62,43 @@ func TestParser(t *testing.T) {
 			}
 
 			p := New(lexer.New(input))
-			actual := p.ParseProgram()
 
-			assert.Nil(t, p.Errors())
-			assert.Equal(t, expect, actual)
+			doTest(t, p, expect)
 		})
 
 	})
 
 }
 
-func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
-	if s.TokenLiteral() != "let" {
-		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
-		return false
+// func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+// 	if s.TokenLiteral() != "let" {
+// 		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+// 		return false
+// 	}
+// 	letStmt, ok := s.(*ast.LetStatement)
+// 	if !ok {
+// 		t.Errorf("s not *ast.LetStatement. got=%T", s)
+// 		return false
+// 	}
+// 	if letStmt.Name.Value != name {
+// 		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+// 		return false
+// 	}
+// 	if letStmt.Name.TokenLiteral() != name {
+// 		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+// 		return false
+// 	}
+// 	return true
+// }
+
+func doTest(t *testing.T, p *Parser, expect interface{}) {
+	actual := p.ParseProgram()
+
+	if errs := p.Errors(); !assert.Nil(t, errs) {
+		for _, err := range errs {
+			t.Log(err)
+		}
+	} else {
+		assert.Equal(t, expect, actual)
 	}
-	letStmt, ok := s.(*ast.LetStatement)
-	if !ok {
-		t.Errorf("s not *ast.LetStatement. got=%T", s)
-		return false
-	}
-	if letStmt.Name.Value != name {
-		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
-		return false
-	}
-	if letStmt.Name.TokenLiteral() != name {
-		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
-		return false
-	}
-	return true
 }
