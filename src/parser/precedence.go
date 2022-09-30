326b75278962d31d1c3ed79ae582ac92199ca271
Aaron Price <aaron.price@hurricanecommerce.com>
2022-09-30 21:24:55 +0100

    simplify precedence

--- src/parser/precedence.go
+++ src/parser/precedence.go
@@ -14,15 +14,15 @@ const (
 )
 
 func (p *Parser) nextPrecedence() int {
-	return p.getPrecedence(p.nextToken)
+	return p.getPrecedence(p.nextToken.Type)
 }
 
 func (p *Parser) crntPrecedence() int {
-	return p.getPrecedence(p.crntToken)
+	return p.getPrecedence(p.crntToken.Type)
 }
 
-func (p *Parser) getPrecedence(t token.Token) int {
-	switch t.Type {
+func (p *Parser) getPrecedence(t token.TokenType) int {
+	switch t {
 	case token.EQ, token.NOT_EQ:
 		return EQUALS
 	case token.LT, token.GT:
