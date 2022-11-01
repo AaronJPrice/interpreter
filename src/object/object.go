df7ba82281d8ddac3fa738f4caa730859b831f1e
Aaron Price <aaron.price@hurricanecommerce.com>
2022-11-01 15:11:59 +0000

    add object package
    	new file:   src/object/object.go

---
+++ src/object/object.go
@@ -0,0 +1,35 @@
+package object
+
+import "fmt"
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type ObjectType string
+
+const (
+	NULL_OBJ    ObjectType = "NULL"
+	BOOLEAN_OBJ            = "BOOLEAN"
+	INTEGER_OBJ            = "INTEGER"
+)
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
