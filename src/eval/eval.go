0c16b1833e3633f21892e300c920b4780d394844
Aaron Price <aaron.price@hurricanecommerce.com>
2024-04-30 16:35:45 +0100

    tidy evaluate func to own file

--- src/eval/eval.go
+++ src/eval/eval.go
@@ -5,18 +5,8 @@ import (
 
 	"bitbucket.org/hurricanecommerce/dev-days/2024-05-09/src/ast"
 	"bitbucket.org/hurricanecommerce/dev-days/2024-05-09/src/eval/object"
-	"bitbucket.org/hurricanecommerce/dev-days/2024-05-09/src/parser"
 	"bitbucket.org/hurricanecommerce/dev-days/2024-05-09/src/token"
 )
-
-func Evaluate(source string) (object.Object, []error) {
-	program, errs := parser.Parse(source)
-	if errs != nil {
-		return nil, errs
-	}
-
-	return evalNode(program), nil
-}
 
 func evalNode(untypedNode ast.Node) object.Object {
 	switch node := untypedNode.(type) {
